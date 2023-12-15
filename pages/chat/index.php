344ce02e05ae2cc47b373c3be3596d103dd5a405
JhonyDev404 <jhonnycan212@gmail.com>
2023-12-15 08:01:33 -0500

    corrección del doble click

--- pages/chat/index.php
+++ pages/chat/index.php
@@ -7,7 +7,6 @@ include_once "$root/chat/services/contacts.php";
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 <link href="js/bootstrap.bundle.min.js" rel="stylesheet" />
 
-
 <div class="container">
     <div class="row clearfix">
         <div class="col-lg-12">
@@ -23,8 +22,8 @@ include_once "$root/chat/services/contacts.php";
 
                     <ul class="list-unstyled chat-list mt-2 mb-0">
                         <?php
-                        
-                    
+
+
                         foreach ($contacts as $contact) {
                             echo <<<HTML
                                     <li class="clearfix" id="contact_$contact[chat]" onclick="selectContact($('#contact_'+$contact[chat]).attr('id'), $('#name_'+$contact[chat]).html(), $('#image_'+$contact[chat]).attr('src'), $_SESSION[id]);">
@@ -35,7 +34,6 @@ include_once "$root/chat/services/contacts.php";
                                         </div>
                                     </li>
                                 HTML;
-
                         }
                         ?>
 
@@ -43,7 +41,6 @@ include_once "$root/chat/services/contacts.php";
                 </div>
 
                 <div class="chat" id="chat">
-
                 </div>
             </div>
         </div>
@@ -54,222 +51,99 @@ include_once "$root/chat/services/contacts.php";
 <script src="/chat/js/mqttws31.min.js" type="text/javascript"></script>
 
 <script>
-// Configuración del cliente MQTT
-const broker = "localhost";
-const port = 8083 //8081; // Puerto por defecto para WebSockets    
-const qos = 1; // Calidad de servicio (QoS)
-var name = "";
-var contacts = [];
-var clients = [];
-var messages = [];
-var currentUser = 0;
+    // Configuración del cliente MQTT
+    const broker = "localhost";
+    const port = 8083 //8081; // Puerto por defecto para WebSockets    
+    const qos = 1; // Calidad de servicio (QoS)
+    var name = "";
+    var contacts = [];
+    var clients = [];
+    // var messages = [];
+    var currentUser = 0;
 
-$(document).ready(function() {
-    url = "/chat/services/getContacts.php";
+    $(document).ready(function() {
+        url = "/chat/services/getContacts.php";
 
-    $.ajax({
-        url: url,
-        type: 'POST',
-        dataType: 'json'
-    }).done(loadContacts).fail(function(response) {
-        console.log("Response: ", response);
-    });
-});
-
-function loadContacts(response) {
-    console.log("Response: ", response);
-    contacts = response.data;
-    console.log("Contacts: ", contacts);
-    console.log("Contacts: ", contacts[0][1]);
-    console.log("Longitud de contactos: ", contacts.length);
-
-    connectContacts();
-}
-
-// Función de conexión
-onConnect = (client) => {
-    console.log("Connect MQTT");
-
-    // Suscribirse al topic
-    topic = "udenar/chat/" + client.clientId;
-    client.subscribe(topic, {
-        qos: qos
-    });
-    console.log('Subscribe to: ' + topic);
-};
-
-// Función de recepción de mensajes
-onMessageArrived = (message) => {
-    console.log("Message arrive: " + message.destinationName + ": " + message.payloadString);
-    client_name = message.destinationName.split('/')[3];
-    console.log(client_name);
-
-    // $('#' + client_name + ' p').html(message.payloadString);
-
-    // if (name != '' && client_name == name) {
-    //     html = $('#chat_msj').html();
-    //     html += "<p>" + message.payloadString + "<p>";
-    //     $('#chat_msj').html(html);
-    // } else {
-    //     $('#chat_msj').html("");
-    // }
-};
-
-// Set many clients
-function connectContacts() {
-    contacts.forEach(function(contact) {
-        var contactId = 'contact_' + contact[0] + "/#";
-
-        var client = new Paho.MQTT.Client(broker, port, contactId);
-        client.onMessageArrived = onMessageArrived;
-        clients.push(client);
-        console.log(clients);
-
-        // Conectar al broker MQTT
-        client.connect({
-            onSuccess: function() {
-                onConnect(client);
-            }, //callback
-            useSSL: false, // Usar conexión segura (SSL)
+        $.ajax({
+            url: url,
+            type: 'POST',
+            dataType: 'json'
+        }).done(loadContacts).fail(function(response) {
+            console.log("Response: ", response);
         });
     });
-};
 
-function messageSent(image, message) {
-    messageContainer = document.getElementById('message-container');
+    function loadContacts(response) {
+        console.log("Response: ", response);
+        contacts = response.data;
+        console.log("Contacts: ", contacts);
+        console.log("Contacts: ", contacts[0][1]);
+        console.log("Longitud de contactos: ", contacts.length);
 
-    messageContainer.innerHTML += `
-            <li class="clearfix">
-                <div class="message-data text-right">
-                    <span class="message-data-time">10:10 AM, Hoy</span>
-                    <img src="${image}" alt="avatar">
-                </div>
-                <div class="message other-message float-right">${message}</div>
-            </li>
-        `;
-}
+        connectContacts();
+    }
 
-function messageReceived(message) {
-    messageContainer = document.getElementById('message-container');
+    // Función de conexión
+    onConnect = (client) => {
+        console.log("Connect MQTT");
 
-    messageContainer.innerHTML += `
-            <li class="clearfix">
-                <div class="message-data">
-                    <span class="message-data-time">10:12 AM, Hoy</span>
-                </div>
-                <div class="message my-message">${message}</div>
-            </li>
-        `;
-}
+        // Suscribirse al topic
+        topic = "udenar/chat/" + client.clientId;
+        client.subscribe(topic, {
+            qos: qos
+        });
+        console.log('Subscribe to: ' + topic);
+    };
 
-function selectContact(id, name, image, user_id) {
-    currentUser = user_id;
-    console.log('Contacto ' + name + ' seleccionado');
-    console.log('image: ' + image);
-    console.log('id: ' + id);
-    loadMessage(id);
-    loadChat(name, image);
+    // Función de recepción de mensajes
+    onMessageArrived = (message) => {
+        console.log("Message arrive: " + message.destinationName + ": " + message.payloadString);
+        client_name = message.destinationName.split('/')[3];
+        console.log(client_name);
 
-    $('.clearfix').removeClass('active');
-    $('#' + id).addClass('active');
-}
-
-// function loadChat(name, image) {
-//     const chat = document.getElementById('chat');
-
-//     chat.innerHTML = `<div class="chat-header clearfix">
-//                     <div class="row">
-//                         <div class="col-lg-6">
-//                             <a href="javascript:void(0);" data-toggle="modal" data-target="#view_info">
-//                                 <img src="${image}" alt="avatar" id='contact_image'>
-//                             </a>
-
-//                             <div class="chat-about">
-//                                 <h6 class="m-b-0" id='contact_name'>${name}</h6>
-//                                 <small>Last seen: 2 hours ago</small>
-//                             </div>
-//                         </div>
-
-//                         <div class="col-lg-6 hidden-sm text-right">
-//                             <a href="javascript:void(0);" class="btn btn-outline-secondary"><i class="fa fa-camera"></i></a>
-//                             <a href="javascript:void(0);" class="btn btn-outline-primary"><i class="fa fa-image"></i></a>
-//                             <a href="javascript:void(0);" class="btn btn-outline-info"><i class="fa fa-cogs"></i></a>
-//                             <a href="javascript:void(0);" class="btn btn-outline-warning"><i class="fa fa-question"></i></a>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <div class="chat-history">
-//                         <ul class="m-b-0" >`;
-//     messages.forEach(function(message) {
-//         //if ((getCookie("user_id"))) {
-//         console.log("user_id : " + currentUser);
-
-//         //}
-//         console.log(message);
-//         if (message[2] == currentUser) {
-//             chat.innerHTML += `
-//                                 <li class="clearfix">
-//                                     <div class="message-data text-right">
-//                                         <span class="message-data-time">${message[1]}</span>
-//                                     </div>
-//                                     <div class="message other-message float-right"> ${message[0]}</div>
-//                                 </li>
-//                         `;
-//         } else {
-//             chat.innerHTML += `<li class="clearfix">
-//                                     <div class="message-data">
-//                                         <span class="message-data-time">${message[1]}</span>
-//                                     </div>
-//                                     <div class="message my-message">${message[0]}</div>
-//                                 </li>`;
-//         }
-//     });
-
-//     chat.innerHTML += `</ul>
-//                 </div>
-//                 <div class="chat-message clearfix">
-//                     <div class="input-group mb-0">
-//                         <div class="input-group-prepend">
-//                             <span class="input-group-text"><i class="fa fa-send"></i></span>
-//                         </div>
-
-//                         <input type="text" class="form-control" placeholder="Enter text here...">
-//                     </div>
-//                 </div>
-//             `;
-// }
-
-function loadChat(name, image) {
-    const chat = document.getElementById('chat');
-    let chat2 = "";
-
-    messages.forEach(function(message) {
-        console.log("user_id : " + currentUser);
-
-        //}
-        console.log(message);
-        if (message[2] == currentUser) {
-            chat2 += `
-                    <li class="clearfix">
-                        <div class="message-data text-right">
-                            <span class="message-data-time">${message[1]}</span>
-                            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="avatar">
-                        </div>
-                        <div class="message other-message float-right"> ${message[0]}</div>
-                    </li>
-            `;
-        } else {
-            chat2 += `
-                <li class="clearfix">
-                    <div class="message-data">
-                        <span class="message-data-time">${message[1]}</span>
-                    </div>
-                    <div class="message my-message">${message[0]}</div>
-                </li>`;
+        if (message.payloadString == 'enviado') {
+            // generar_numeros();
+            console.log(message.payloadString);
         }
-    });
+    };
 
-    chat.innerHTML = `
+    // Set many clients
+    function connectContacts() {
+        contacts.forEach(function(contact) {
+            var contactId = 'contact_' + contact[0] + "/#";
+
+            var client = new Paho.MQTT.Client(broker, port, contactId);
+            client.onMessageArrived = onMessageArrived;
+            clients.push(client);
+            console.log(clients);
+
+            // Conectar al broker MQTT
+            client.connect({
+                onSuccess: function() {
+                    onConnect(client);
+                }, //callback
+                useSSL: false, // Usar conexión segura (SSL)
+            });
+        });
+    };
+
+    function selectContact(id, name, image, user_id) {
+        currentUser = user_id;
+        console.log('Contacto ' + name + ' seleccionado');
+        console.log('image: ' + image);
+        console.log('id: ' + id);
+
+        loadChat(name, image);
+        loadMessage(id);
+
+        $('.clearfix').removeClass('active');
+        $('#' + id).addClass('active');
+    }
+
+    function loadChat(name, image) {
+        const chat = document.getElementById('chat');
+
+        chat.innerHTML = `
                 <div class="chat-header clearfix">
                     <div class="row">
                         <div class="col-lg-6">
@@ -294,7 +168,6 @@ function loadChat(name, image) {
 
                 <div class="chat-history">
                     <ul class="m-b-0" id="message-container">
-                    ${chat2}
                     </ul>
                 </div>
 
@@ -308,24 +181,59 @@ function loadChat(name, image) {
                     </div>
                 </div>
             `;
-}
+    }
 
-function loadMessage(chat_id) {
+    function messageSent(image, message, date) {
+        messageContainer = document.getElementById('message-container');
 
-    id = chat_id.split('_')[1];
-    url = "/chat/services/getMessages.php";
+        messageContainer.innerHTML += `
+            <li class="clearfix">
+                <div class="message-data text-right">
+                    <span class="message-data-time">${date}</span>
+                    <img src="${image}" alt="avatar">
+                </div>
+                <div class="message other-message float-right">${message}</div>
+            </li>
+        `;
+    }
 
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: {
-            id: id
-        },
-        dataType: 'json'
-    }).done(function(response) {
-        messages = response.data;
-    }).fail(function(response) {
-        console.log("Response: ", response);
-    });
-}
+    function messageReceived(message, date) {
+        messageContainer = document.getElementById('message-container');
+
+        messageContainer.innerHTML += `
+            <li class="clearfix">
+                <div class="message-data">
+                    <span class="message-data-time">${date}</span>
+                </div>
+                <div class="message my-message">${message}</div>
+            </li>
+        `;
+    }
+
+    function loadMessage(chat_id) {
+        id = chat_id.split('_')[1];
+        messages = [];
+        url = "/chat/services/getMessages.php";
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {
+                id: id
+            },
+            dataType: 'json'
+        }).done(function(response) {
+            messages = response.data;
+
+            messages.forEach(function(message) {
+                if (message[2] == currentUser) {
+                    messageSent("https://bootdey.com/img/Content/avatar/avatar1.png", message[0], message[1]);
+                } else {
+                    messageReceived(message[0], message[1])
+                }
+            });
+        }).fail(function(response) {
+            console.log("Response: ", response);
+        });
+    }
 </script>
